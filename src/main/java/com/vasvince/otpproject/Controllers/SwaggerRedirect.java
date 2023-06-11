bcba3e17856ab3638c9fd25333ea907aa08f7cd1
vasvince98 <87307436+vasvince98@users.noreply.github.com>
2023-06-11 23:33:46 +0200

    swagger-ui redirection added

---
+++ src/main/java/com/vasvince/otpproject/Controllers/SwaggerRedirect.java
@@ -0,0 +1,14 @@
+package com.vasvince.otpproject.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class SwaggerRedirect {
+
+    @RequestMapping("/swagger-ui")
+    public RedirectView redirectWithUsingRedirectView() {
+        return new RedirectView("/swagger-ui.html");
+    }
+}
